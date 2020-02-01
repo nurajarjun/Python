24cf80b3e3f8c0ee96852fdf70d2534b4ddfcc4c
VivekanandanN <Vivekn707@gmail.com>
2020-01-31 16:16:33 -0800

    removed unstandard files - related to section 2

--- section2_1.py
+++
@@ -1,7 +0,0 @@
-intInputValue = int(input())
-if intInputValue >= 0 and intInputValue <=1000:
-    print("Saver")
-elif intInputValue >= 1001 and intInputValue <=10000:
-    print("Economy")
-elif intInputValue >=10001:
-    print("Flexi")
